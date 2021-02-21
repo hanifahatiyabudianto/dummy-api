11a73ddb517d89fe5acb289eaa453712797e080c
Hendri Antomy <hendri.antomy@mokapos.com>
2021-02-21 19:48:49 +0700

    initial commit

---
+++ src/main/java/tech/atlasid/dummyapiautomation/DummyApiAutomationApplication.java
@@ -0,0 +1,13 @@
+package tech.atlasid.dummyapiautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApiAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyApiAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/atlasid/dummyapiautomation/DummyApiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.atlasid.dummyapiautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApiAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
